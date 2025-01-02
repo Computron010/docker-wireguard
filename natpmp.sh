4ac12e755a88f7b98de4382b483a74d11308d051
Computron010 <56774862+Computron010@users.noreply.github.com>
2025-01-02 14:58:02 -0800

    Update natpmp.sh

--- natpmp.sh
+++ natpmp.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 NATPMP_IP="$1"
 
-while true; do
-    RES=$(natpmpc -a 1 0 udp 60 -g "$NATPMP_IP" && natpmpc -a 1 0 tcp 60 -g "$NATPMP_IP")
+# Run natpmpc once to populate files
+RES=$(natpmpc -a 1 0 udp 60 -g "$NATPMP_IP" && natpmpc -a 1 0 tcp 60 -g "$NATPMP_IP")
 
+if [ $? -ne 0 ]; then
+    echo -e "ERROR with natpmpc command \a"
+    break
+fi
+
+if [ ! -f /shared/port.dat ]; then
+    PORT=$(echo $RES | grep 'Mapped public port' | grep 'protocol TCP' | awk '{print $4}')
+    PUBLIC_IP=$(echo $RES | grep 'Public IP address' | awk '{print $NF}' | head -1)
+
+    echo "$PORT" > /shared/port.dat
+    echo "$PUBLIC_IP" > /shared/public_ip.dat
+fi
+
+sleep 45
+while true; do
+    natpmpc -a 1 0 udp 60 -g "$NATPMP_IP" && natpmpc -a 1 0 tcp 60 -g "$NATPMP_IP"
+    
     if [ $? -ne 0 ]; then
         echo -e "ERROR with natpmpc command \a"
         break
     fi
-
-    echo "$RES" > /tmp/natpmp_output
-
+    
     sleep 45
 done
